1dfdab3cc2597aa348017dfa2719ccffc734f227
Arkopravo Das <arko.andrew2000@gmail.com>
2024-02-23 04:12:38 +0530

    new logo added

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -7,8 +7,10 @@ function Topbar() {
   return (
     <nav className="topbar">
       <Link href="/" className="flex items-center gap-4">
-        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
-        <p className="text-heading3-bold text-light-1 max-xs:hidden">ThreadX</p>
+        <Image src="/assets/logo3.svg" alt="logo" width={35} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden font-sans">
+          ThreadX
+        </p>
       </Link>
 
       <div className="flex items-center gap-1">
